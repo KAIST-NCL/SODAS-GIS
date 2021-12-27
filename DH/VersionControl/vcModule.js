3ba7def0d33d553dcfdd5cb408a608415136b06c
korjwl1 <72062804+korjwl1@users.noreply.github.com>
2021-12-27 15:46:42 +0900

    Update vcModule.js

--- DH/VersionControl/vcModule.js
+++ DH/VersionControl/vcModule.js
@@ -9,7 +9,8 @@ exports.vcModule = function(){
     ////////////////////////////////////////////////////////////////////////////////////////////////
     console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
     console.log("vcModule created");
-    console.log("vcModule - workerData: " + workerData);
+    console.log("vcModule - workerData: ");
+    console.log(workerData);
     console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
     ////////////////////////////////////////////////////////////////////////////////////////////////
     const gitDir = workerData.pubvc_root;
@@ -18,15 +19,16 @@ exports.vcModule = function(){
     this.smPort = workerData.sm_port;
     this.vc = new publishVC(gitDir, workerData.rmsync_root_dir);
     this.consumer = new vcConsumer(kafkaHost, options, this);
-    this.flag = workerData.flag; // mutex flag
+    this.flag = workerData.mutex_flag; // mutex flag
     var self = this;
     parentPort.on('message', message => {
         console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
-        console.log("vcModule Received message: " + message);
+        console.log("vcModule Received message: ");
+        console.log(message);
         console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
         switch(message.event) {
             case 'UPDATE_REFERENCE_MODEL':
-                self.vc.addReferenceModel(message.RM);
+                self.vc.addReferenceModel(message.data);
         }
     });
 };
