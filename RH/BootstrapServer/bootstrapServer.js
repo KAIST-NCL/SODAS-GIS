431fbd3021455572bc7b6a86340ee4b50990e2ef
EunjuYang <yejyang@kaist.ac.kr>
2022-03-10 20:04:56 +0900

    change console.log into debug

--- RH/BootstrapServer/bootstrapServer.js
+++ RH/BootstrapServer/bootstrapServer.js
@@ -1,8 +1,8 @@
-
 const PROTO_PATH = __dirname+'/proto/bootstrap.proto';
 const bs = require(__dirname+'/bootstrapServer');
 const grpc = require('@grpc/grpc-js');
-const protoLoader = require('@grpc/proto-loader')
+const protoLoader = require('@grpc/proto-loader');
+const debug = require('debug')('sodas:bootstrap_server');
 
 exports.BootstrapServer = function () {
 
@@ -18,27 +18,27 @@ exports.BootstrapServer = function () {
     this.BSproto = this.protoDescriptor.bootstrap.BootstrapBroker;
     this.seedNodeList = []
 
-}
+};
 
 exports.BootstrapServer.prototype._setSeedNode = function (call, callback) {
-    console.log("SetSeedNode")
-    var seedNode = call.request
-    console.log(seedNode)
-    bsServer.seedNodeList.unshift(seedNode)
-    console.log(bsServer.seedNodeList)
+    debug("SetSeedNode");
+    var seedNode = call.request;
+    debug(seedNode);
+    bsServer.seedNodeList.unshift(seedNode);
+    debug(bsServer.seedNodeList);
     callback(null, { status: true, message: "Success enroll node info" })
-}
+};
 
 exports.BootstrapServer.prototype._getSeedNodeList = function (call, callback) {
-    console.log("[RH] [Bootstrap Server] - GetSeedNodeList")
-    var seedNode = call.request
-    console.log("Access Bootstrap Server from")
-    console.log(seedNode)
-    callback(null, {nodes: bsServer.seedNodeList})
-    bsServer.seedNodeList.unshift(seedNode)
-    console.log("SeedNodeList is")
-    console.log(bsServer.seedNodeList)
-}
+    debug("[RH] [Bootstrap Server] - GetSeedNodeList");
+    var seedNode = call.request;
+    debug("Access Bootstrap Server from");
+    debug(seedNode);
+    callback(null, {nodes: bsServer.seedNodeList});
+    bsServer.seedNodeList.unshift(seedNode);
+    debug("SeedNodeList is");
+    debug(bsServer.seedNodeList);
+};
 
 exports.BootstrapServer.prototype._setBootstrapServer = function () {
     this.server = new grpc.Server();
@@ -47,16 +47,16 @@ exports.BootstrapServer.prototype._setBootstrapServer = function () {
         GetSeedNodeList: this._getSeedNodeList
     });
     return this.server;
-}
+};
 
 exports.BootstrapServer.prototype.run = function () {
     this.bootstrapServer = this._setBootstrapServer();
     this.bootstrapServer.bindAsync(this.bootstrapServerIP,
         grpc.ServerCredentials.createInsecure(), () => {
-            console.log('Bootstrap Server gRPC Server running at ' + this.bootstrapServerIP)
+            debug('Bootstrap Server gRPC Server running at ' + this.bootstrapServerIP)
             this.bootstrapServer.start();
         });
-}
+};
 
-const bsServer = new bs.BootstrapServer()
-bsServer.run()
+const bsServer = new bs.BootstrapServer();
+bsServer.run();
