9417453ba3a1deb5a6a6fd7cfb1e9e07df3945e8
dinhkt <dinhkhactuyen97@gmail.com>
2022-04-28 14:26:31 +0900

    Clean code and refactor

--- RH/VersionControl/versionController.js
+++ RH/VersionControl/versionController.js
@@ -2,7 +2,6 @@ const { Git } = require(__dirname + '/../../DH/Lib/git');
 const debug = require('debug')('sodas:versionController');
 const fs= require('fs');
 class VC {
-
     static Flag = false;
     static FirstCommit = 'asdfasdf';
 
@@ -16,6 +15,7 @@ class VC {
         var value = '';
         await this.git.init()
             .then((commnum) => {
+                //Create 2 folders for each rdf type
                 value = (' ' + commnum).slice(1);
                 fs.mkdir(this.vcRoot+'/'+'domain', function(err) {
                     if (err) {
@@ -30,30 +30,26 @@ class VC {
         this.isInit = true;
         return value;
     }
-
     returnFirstCommit(self, dir) {
         return self.git.getInitCommit(dir);
     }
 }
 
 class publishVC extends VC{
-    // static class variable (mutex)
     constructor(RMgitDir) {
         super(RMgitDir);
     }
     async commit(filepath, message, vm){
-        // Flag가 0이면 열린 상태, 1이면 잠긴 상태
+        // Flag=1 means not be able to commit, 0 means be able to commit
         if(vm.flag[0] == 1){
-            // retry commit
+            // retry commit after timeout
             const timeOut = 100;
             setTimeout(this.commit.bind(this), timeOut, filepath, message, vm);
         }else{
-            // MUTEX ON
             vm.lockMutex(vm);
             debug('[PublishVC Event]: ' + message + ' - bind because of Mutex');
             var commitNum = await this.git.commit(filepath, message);
             debug(commitNum);
-            // MUTEX OFF
             vm.unlockMutex(vm);
             debug('[PublishVC Event]: ' + message + ' - Mutex unlock');
             if (commitNum != '') vm.reportCommit(vm, commitNum);
@@ -61,22 +57,5 @@ class publishVC extends VC{
     }
 }
 
-class subscribeVC extends VC{
-    constructor(RMgitDir) {
-        super(RMgitDir);
-    }
-    
-    async commit(filepath, message){
-        // commit 처리할 때 mutex 없게 수정해야함
-        await this.git.commit(filepath, message);
-    }
-
-    apply(gitPatch) {
-        this.git.apply(gitPatch);
-    }
-}
-
-
 exports.VC = VC;
 exports.publishVC = publishVC;
-exports.subscribeVC = subscribeVC;
